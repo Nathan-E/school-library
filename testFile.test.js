f7771348d27de531b408a7bf8bc02d0d965f8255
Chibueze <eziokwubunduchibueze@gmail.com>
2019-01-09 15:57:11 +0100

    Admin constructor function test case failed

--- testFile.test.js
+++ testFile.test.js
@@ -1,10 +1,20 @@
 const User = require('./user');
+const Admin = require('./admin');
 
-//Tests the Library Function
+//Tests the User function
 describe('1. Tests the User Module', () => {
     test('User constructor', () => {
         const obj = new User('Chibueze', 'Admin');
         expect(obj.name).toBe('Chibueze');
+        expect(obj.type).toBe('Admin');
+    });
+});
+
+//Tests the Admin Function
+describe('2. Tests the Admin Module', () => {
+    test('Admin constructor', () => {
+        const obj = new Admin('Zik', 'Admin');
+        expect(obj.name).toBe('Zik');
         expect(obj.type).toBe('Admin');
     });
 });
