d89e3469b56956122cb258ff2bc66ee9392bdd8d
Chibueze <eziokwubunduchibueze@gmail.com>
2019-01-10 16:15:09 +0100

    Teacher, Senior and Junior req can be differentiated

--- user.js
+++ user.js
@@ -1,11 +1,5 @@
 const Person = require('./person');
 const extend = require('./extend');
-const Teacher = require('./teacher');
-const Senior = require('./senior');
-const bookRequest = require('./request');
-const teacherArray = require('');
-const seniorArray = require('');
-const juniorArray = require('');
 
 //User constructor (Parent function)
 function User(name) {
@@ -13,18 +7,5 @@ function User(name) {
 }
 
 extend(User, Person);
-
-//req function sent a book request to the bookRequest array
-User.prototype.req = function (name){
-    if(Teacher.prototype.isPrototypeOf(this)) {
-        teacherArray.push(name);
-      }
-      else if(Senior.prototype.isPrototypeOf(this)){
-        seniorArray.push(name);
-      }
-      else{
-        juniorArray.push(name);
-      }
-}
 
 module.exports = User;
