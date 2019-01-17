fbf3cc45f1b74ae9279476187b49485fe2483ecb
Chibueze <eziokwubunduchibueze@gmail.com>
2019-01-17 15:23:46 +0100

    Test cases written for senior.js

--- person/users/students/senior/senior.test.js
+++ person/users/students/senior/senior.test.js
@@ -1,3 +1,8 @@
+const Senior = require('./senior');
 
+const Dawid = new Senior('John', 4234);
 
-
+//Checks if User is an Instance of Teacher
+test('Checks if David is an Instance of Teacher', () => {
+    expect(Dawid).toBeInstanceOf(Senior);
+});
